ade82d5eab2eb28004f0270ee02a7f917acdad10
Huahai Yang <huahai.yang@gmail.com>
2020-06-14 22:19:06 -0700

    tests pass

--- project.clj
+++ project.clj
@@ -1,12 +1,18 @@
-(defproject datalightning "0.1.0-SNAPSHOT"
+(defproject datalevin "0.1.0-SNAPSHOT"
   :description "A port of Datascript to LMDB"
-  :url "https://github.com/juji-io/datalightning"
+  :url "https://github.com/juji-io/datalevin"
   :license {:name "EPL-1.0"
             :url "https://www.eclipse.org/legal/epl-1.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"]
+                 [persistent-sorted-set     "0.1.2"]
                  [org.lmdbjava/lmdbjava "0.8.1"]
                  [com.taoensso/nippy "2.15.0-RC1"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "1.0.0"]
                                   [criterium "0.4.5"]
                                   [com.taoensso/timbre "4.10.0"]]}}
-  :warn-on-reflection true)
+  :global-vars {*warn-on-reflection*   true
+                *print-namespace-maps* false
+                ;;     *unchecked-math* :warn-on-boxed
+                })
