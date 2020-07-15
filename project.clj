d3c02cb7870dfe67f63d66f3544c3d1df41cb955
Huahai Yang <huahai.yang@gmail.com>
2020-07-15 12:34:13 -0700

    Version 0.1.8

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(def version "0.1.6")
+(def version "0.1.8")
 
 (defproject datalevin version
   :description "A port of Datascript to LMDB"
   :url "https://github.com/juji-io/datalevin"
   :license {:name "EPL-1.0"
-            :url "https://www.eclipse.org/legal/epl-1.0/"}
+            :url  "https://www.eclipse.org/legal/epl-1.0/"}
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [persistent-sorted-set "0.1.2"]
                  [org.lmdbjava/lmdbjava "0.8.1"]
@@ -14,6 +14,11 @@
                                   [com.taoensso/timbre "4.10.0"]]}}
   :jvm-opts ["--add-opens" "java.base/java.nio=ALL-UNNAMED"
              "--add-opens" "java.base/sun.nio.ch=ALL-UNNAMED"]
+
+  :deploy-repositories [["clojars" {:url           "https://repo.clojars.org"
+                                    :username      :env/clojars_username
+                                    :password      :env/clojars_password
+                                    :sign-releases false}]]
   :global-vars {*warn-on-reflection*   true
                 *print-namespace-maps* false
                 ;; *unchecked-math* :warn-on-boxed
