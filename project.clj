194c4ebca8f0fe678a881b38c4f5546f89de0627
Huahai Yang <huahai.yang@gmail.com>
2020-07-16 15:33:51 -0700

    update project description

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def version "0.2.0")
 
 (defproject datalevin version
-  :description "A port of Datascript to LMDB"
+  :description "[Datalevin](https://github.com/juji-io/datalevin) is a port of Datascript to LMDB"
   :url "https://github.com/juji-io/datalevin"
   :license {:name "EPL-1.0"
             :url  "https://www.eclipse.org/legal/epl-1.0/"}
@@ -9,11 +9,11 @@
                  [persistent-sorted-set "0.1.2"]
                  [org.lmdbjava/lmdbjava "0.8.1"
                   ;; uncomment when run lein codox
-                  ;; :exclusions
-                  ;; [org.ow2.asm/asm-analysis
-                  ;;  org.ow2.asm/asm-commons
-                  ;;  org.ow2.asm/asm-tree
-                  ;;  org.ow2.asm/asm-util]
+                  :exclusions
+                  [org.ow2.asm/asm-analysis
+                   org.ow2.asm/asm-commons
+                   org.ow2.asm/asm-tree
+                   org.ow2.asm/asm-util]
                   ]
                  [com.taoensso/nippy "2.14.0"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "1.0.0"]
