69fe518528dc1c0df0a497cb550c458185110030
Huahai Yang <huahai.yang@gmail.com>
2020-07-26 00:42:06 -0700

    update description

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def version "0.2.8")
 
 (defproject datalevin version
-  :description "A port of Datascript to LMDB"
+  :description "A simple, fast and durable Datalog database"
   :url "https://github.com/juji-io/datalevin"
   :license {:name "EPL-1.0"
             :url  "https://www.eclipse.org/legal/epl-1.0/"}
