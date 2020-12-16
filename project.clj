6e5eb4dc5ab1fb97e0f0fde45190b24e2a35bb7a
Huahai Yang <huahai.yang@gmail.com>
2020-12-15 16:54:50 -0800

    update

--- project.clj
+++ project.clj
@@ -10,11 +10,11 @@
                  [com.taoensso/nippy "3.0.0"]
                  [org.lmdbjava/lmdbjava "0.8.1"
                   ;; uncomment when run lein codox
-                  ;; :exclusions
-                  ;; [org.ow2.asm/asm-analysis
-                  ;;  org.ow2.asm/asm-commons
-                  ;;  org.ow2.asm/asm-tree
-                  ;;  org.ow2.asm/asm-util]
+                  :exclusions
+                  [org.ow2.asm/asm-analysis
+                   org.ow2.asm/asm-commons
+                   org.ow2.asm/asm-tree
+                   org.ow2.asm/asm-util]
                   ]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.0"]
                                   [com.taoensso/timbre "5.0.1"]]}}
