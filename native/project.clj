d9651a0d7099f54dc244008de9982201a9398a40
Huahai Yang <huahai.yang@gmail.com>
2021-03-04 13:55:14 -0800

    Version 0.4.18

--- native/project.clj
+++ native/project.clj
@@ -1,4 +1,4 @@
-(def version "0.4.17")
+(def version "0.4.18")
 
 (defproject datalevin-native version
   :description "Datalevin running in GraalVM native image, and it can also run as a command line tool for Datalevin"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def version "0.4.17")
+(def version "0.4.18")
 
 (defproject datalevin version
   :description "A simple, fast and durable Datalog database"
