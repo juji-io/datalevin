3dde90585ccc3e42f0b5722c902e9e189a62d476
Huahai Yang <huahai.yang@gmail.com>
2021-03-10 10:03:57 -0800

    try cmake

--- native/script/compile.bat
+++ native/script/compile.bat
@@ -15,6 +15,17 @@ set CPATH=%PWD%\src\c
 call ..\lein.bat do clean, uberjar
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+cd %CPATH%\lmdb\libraries\liblmdb
+
+mkdir build
+cd build
+cmake .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE:STRING=RELEASE ^
+    -DCMAKE_INSTALL_PREFIX=%CPATH% ^
+    -DCLOSE_WARNING=on ^
+    -DBUILD_TEST=off ^
+    -DBUILD_SHARED_LIBS=off
+nmake install
+
 cd %CPATH%
 make -f Makefile.win
 
@@ -28,6 +39,7 @@ call %GRAALVM_HOME%\bin\native-image.cmd ^
   "-J-Dclojure.spec.skip-macros=true" ^
   "-J-Dclojure.compiler.direct-linking=true" ^
   "-H:CLibraryPath=%CPATH%" ^
+  "-H:NativeLinkerOption=legacy_stdio_definitions.lib" ^
   "--initialize-at-build-time"  ^
   "-H:Log=registerResource:" ^
   "--report-unsupported-elements-at-runtime" ^
