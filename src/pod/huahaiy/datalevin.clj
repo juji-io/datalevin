4179b86713017e747596e237a2b2ad158847efc9
Huahai Yang <huahai.yang@gmail.com>
2021-03-15 21:53:09 -0700

    bb pod seems to work

--- src/pod/huahaiy/datalevin.clj
+++ src/pod/huahaiy/datalevin.clj
@@ -4,9 +4,10 @@
             [datalevin.core :as d]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
-            [clojure.string :as s])
+            [clojure.walk :as w])
   (:import [java.io PushbackInputStream]
-           [java.nio.charset StandardCharsets])
+           [java.nio.charset StandardCharsets]
+           [java.util UUID])
   (:gen-class))
 
 (def pod-ns "pod.huahaiy.datalevin")
@@ -29,20 +30,45 @@
 (defn read []
   (bencode/read-bencode stdin))
 
-(def dl-conns (atom {}))
-(def dl-dbs (atom {}))
+;; uuid -> conn
+(defonce dl-conns (atom {}))
 
-(def kv-dbs (atom {}))
+;; uuid -> conn
+(defonce dl-dbs (atom {}))
 
-(defn- get-conn [])
+(defonce kv-dbs (atom {}))
 
-(defn- close [])
+(defn- get-conn
+  ([dir]
+   (get-conn dir nil))
+  ([dir schema]
+   (let [conn (d/get-conn dir schema)
+         id   (UUID/randomUUID)]
+     (swap! dl-conns assoc id conn)
+     {::conn id})))
 
-(defn- transact! [])
+(defn- close [{:keys [::conn]}]
+  (let [[old _] (swap-vals! dl-conns dissoc conn)]
+    (when-let [c (get old conn)]
+      (d/close c))))
 
-(defn- db [])
+(defn- transact!
+  ([conn tx-data]
+   (transact! conn tx-data nil))
+  ([{:keys [::conn]} tx-data tx-meta]
+   (when-let [c (get @dl-conns conn)]
+     (d/transact! c tx-data tx-meta))))
 
-(defn- q [])
+(defn- db [{:keys [::conn]}]
+  (when-let [c (get @dl-conns conn)]
+    (let [d  (d/db c)
+          id (UUID/randomUUID)]
+      (swap! dl-dbs assoc id d)
+      {::db id})))
+
+(defn- q [q & inputs]
+  (apply d/q q (w/postwalk #(if-let [db (::db %)] (get @dl-dbs db) %)
+                           inputs)))
 
 (def exposed-vars
   {'get-conn  get-conn
@@ -67,8 +93,8 @@
             :describe (do (write {"format"     "edn"
                                   "namespaces" [{"name" "pod.huahaiy.datalevin"
                                                  "vars"
-                                                 (mapv (fn [v] {"name" (name v)})
-                                                       exposed-vars)}]
+                                                 (mapv (fn [k] {"name" (name k)})
+                                                       (keys exposed-vars))}]
                                   "id"         id})
                           (recur))
             :invoke   (do (try
