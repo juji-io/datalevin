e3602aa36342727b408e3521a874072c5aed7045
Huahai Yang <huahai.yang@gmail.com>
2021-09-10 07:56:59 -0700

    overwrite existing binary as well

--- src/datalevin/search.clj
+++ src/datalevin/search.clj
@@ -2,6 +2,7 @@
   "Full-text search engine"
   (:require [clojure.string :as s]
             [datalevin.lmdb :as l]
+            [datalevin.constants :as c]
             [datalevin.bits :as b]
             [datalevin.constants :as c]))
 
@@ -10,7 +11,11 @@
   (def env (l/open-kv "/tmp/search"))
 
   (def dbi (l/open-dbi env "dict" c/+max-key-size+ c/+default-val-size+
-                       (conj )))
+                       (conj c/default-dbi-flags :dupsort)))
 
+  (l/transact-kv env [[:put "dict" "a" 1]
+                      [:put "dict" "a" 2]])
+
+  (l/get-value env "dict" "a")
 
   )
