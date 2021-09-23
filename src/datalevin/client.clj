147e1e4c9352fe9c6ce9c1a9f12a7ff008c35361
Huahai Yang <huahai.yang@gmail.com>
2021-09-22 18:40:22 -0700

    lock the buffer

--- src/datalevin/client.clj
+++ src/datalevin/client.clj
@@ -23,12 +23,12 @@
   IConnection
   (send-n-receive [this msg]
     (try
-      (p/write-message-blocking ch bf msg)
-      (.clear bf)
-      (println "is ch blocking?" (.isBlocking ch))
-      (let [[resp bf'] (p/receive-ch ch bf)]
-        (when-not (identical? bf' bf) (set! bf bf'))
-        resp)
+      (locking bf
+        (p/write-message-blocking ch bf msg)
+        (.clear bf)
+        (let [[resp bf'] (p/receive-ch ch bf)]
+          (when-not (identical? bf' bf) (set! bf bf'))
+          resp))
       (catch BufferOverflowException _
         (let [size (* c/+buffer-grow-factor+ ^int (.capacity bf))]
           (set! bf (b/allocate-buffer size))
