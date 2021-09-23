0d09b8986f627701775b221677cf650d7aaddf41
Huahai Yang <huahai.yang@gmail.com>
2021-09-22 22:35:46 -0700

    Version 0.5.19

--- native/project.clj
+++ native/project.clj
@@ -1,4 +1,4 @@
-(def version "0.5.18")
+(def version "0.5.19")
 
 (defproject org.clojars.huahaiy/datalevin-native version
   :description "Datalevin GraalVM native image and command line tool"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def version "0.5.18")
+(def version "0.5.19")
 
 (defproject datalevin version
   :description "A simple, fast and versatile Datalog database"
