9410854b6aed291cd4a84d68294a63013b65ac18
Huahai Yang <huahai.yang@gmail.com>
2021-10-16 21:49:48 -0700

    wip testing"

--- test/datalevin/search_test.clj
+++ test/datalevin/search_test.clj
@@ -3,7 +3,7 @@
             [datalevin.lmdb :as l]
             [datalevin.constants :as c]
             [datalevin.util :as u]
-            [clojure.test :refer [is deftest]])
+            [clojure.test :refer [is deftest testing]])
   (:import [java.util UUID Map ArrayList]
            [datalevin.sm SymSpell Bigram]
            [datalevin.search SearchEngine]))
@@ -17,7 +17,7 @@
     (is (= (subs s1 10 (+ 10 (.length "datalevin-analyzers")))
            "Datalevin-Analyzers" ))))
 
-(deftest index-test
+(deftest basic-ops-test
   (let [lmdb   (l/open-kv (u/tmp-dir (str "index-" (UUID/randomUUID))))
         engine ^SearchEngine (sut/new-engine lmdb)]
     (sut/add-doc engine :doc1
@@ -30,47 +30,50 @@
                  "The robber wore a red fleece jacket and a baseball cap.")
     (sut/add-doc engine :doc5
                  "The English Springer Spaniel is the best of all red dogs.")
-    (is (= (count (.-unigrams engine))
-           (l/range-count lmdb c/unigrams [:all] :string)
-           (.size ^Map (.getUnigramLexicon ^SymSpell (.-symspell engine)))
-           30))
-    (let [[tid freq] (l/get-value lmdb c/unigrams "red" :string :id-id true)]
-      (is (= freq
-             (.get ^Map (.getUnigramLexicon ^SymSpell (.-symspell engine)) "red")
-             5))
-      (is (= (.get ^Map (.-terms engine) tid) "red"))
-      (is (l/in-list? lmdb c/term-docs tid 1 :id :id))
-      (is (l/in-list? lmdb c/term-docs tid 5 :id :id))
-      (is (= (l/list-count lmdb c/term-docs tid :id) 4))
-      (is (= (l/get-list lmdb c/term-docs tid :id :id) [1 2 4 5]))
-      (is (= (l/list-count lmdb c/positions [1 tid] :id-id) 2))
-      (is (= (l/list-count lmdb c/positions [5 tid] :id-id) 1))
-      (is (= (l/get-list lmdb c/positions [5 tid] :id-id :int-int)
-             [[9 48]]))
-      (is (= (l/range-count lmdb c/positions [:closed [5 0] [5 Long/MAX_VALUE]]
-                            :id-id)
-             7))
-      (let [[tid2 freq2] (l/get-value lmdb c/unigrams "dogs"
-                                      :string :id-id true)]
-        (is (= freq2 2))
-        (is (= (l/get-value lmdb c/bigrams [tid tid2] :id-id :id true)
-               (.get ^Map (.getBigramLexicon ^SymSpell (.-symspell engine))
-                     (Bigram. "red" "dogs"))
-               2)))
 
-      (is (= (l/get-value lmdb c/docs 1 :id :data true) {:ref :doc1 :uniq 7}))
-      (is (= (l/get-value lmdb c/docs 4 :id :data true) {:ref :doc4 :uniq 7}))
-      (is (= (l/get-value lmdb c/rdocs :doc4 :data :id true) 4))
-      (is (= (l/range-count lmdb c/docs [:all]) 5))
-      (is (= (l/range-count lmdb c/rdocs [:all]) 5))
+    (testing "indexing"
+      (is (= (count (.-unigrams engine))
+             (l/range-count lmdb c/unigrams [:all] :string)
+             (.size ^Map (.getUnigramLexicon ^SymSpell (.-symspell engine)))
+             30))
+      (let [[tid freq] (l/get-value lmdb c/unigrams "red" :string :id-id true)]
+        (is (= freq
+               (.get ^Map (.getUnigramLexicon ^SymSpell (.-symspell engine)) "red")
+               5))
+        (is (= (.get ^Map (.-terms engine) tid) "red"))
+        (is (l/in-list? lmdb c/term-docs tid 1 :id :id))
+        (is (l/in-list? lmdb c/term-docs tid 5 :id :id))
+        (is (= (l/list-count lmdb c/term-docs tid :id) 4))
+        (is (= (l/get-list lmdb c/term-docs tid :id :id) [1 2 4 5]))
+        (is (= (l/list-count lmdb c/positions [1 tid] :id-id) 2))
+        (is (= (l/list-count lmdb c/positions [5 tid] :id-id) 1))
+        (is (= (l/get-list lmdb c/positions [5 tid] :id-id :int-int)
+               [[9 48]]))
+        (is (= (l/range-count lmdb c/positions [:closed [5 0] [5 Long/MAX_VALUE]]
+                              :id-id)
+               7))
+        (let [[tid2 freq2] (l/get-value lmdb c/unigrams "dogs"
+                                        :string :id-id true)]
+          (is (= freq2 2))
+          (is (= (l/get-value lmdb c/bigrams [tid tid2] :id-id :id true)
+                 (.get ^Map (.getBigramLexicon ^SymSpell (.-symspell engine))
+                       (Bigram. "red" "dogs"))
+                 2)))
 
-      (sut/remove-doc engine :doc5)
-      (is (= (l/range-count lmdb c/docs [:all]) 4))
-      (is (= (l/range-count lmdb c/rdocs [:all]) 4))
-      (is (not (l/in-list? lmdb c/term-docs tid 5 :id :id)))
-      (is (= (l/list-count lmdb c/term-docs tid :id) 3))
-      (is (= (l/list-count lmdb c/positions [5 tid] :id-id) 0))
-      (is (= (l/get-list lmdb c/positions [5 tid] :id-id :int-int) [])))
+        (is (= (l/get-value lmdb c/docs 1 :id :data true) {:ref :doc1 :uniq 7}))
+        (is (= (l/get-value lmdb c/docs 4 :id :data true) {:ref :doc4 :uniq 7}))
+        (is (= (l/get-value lmdb c/rdocs :doc4 :data :id true) 4))
+        (is (= (l/range-count lmdb c/docs [:all]) 5))
+        (is (= (l/range-count lmdb c/rdocs [:all]) 5))
+
+        (sut/remove-doc engine :doc5)
+        (is (= (l/range-count lmdb c/docs [:all]) 4))
+        (is (= (l/range-count lmdb c/rdocs [:all]) 4))
+        (is (not (l/in-list? lmdb c/term-docs tid 5 :id :id)))
+        (is (= (l/list-count lmdb c/term-docs tid :id) 3))
+        (is (= (l/list-count lmdb c/positions [5 tid] :id-id) 0))
+        (is (= (l/get-list lmdb c/positions [5 tid] :id-id :int-int) [])))
+      )
 
     (l/close-kv lmdb)
     ))
