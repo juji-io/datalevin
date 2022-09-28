3d0ac6229b62d4c4ffee68bb64b411d3e49ec0ca
Eugen Stan <eugen.stan@netdava.com>
2022-09-28 23:57:40 +0300

    [#98] datalevin is usable in deps.edn via gitlibs
    * Updated deps.edn

---
+++ build.clj
@@ -0,0 +1,14 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn compile-java [_]
+  (b/javac {:src-dirs ["src/java"]
+            :class-dir class-dir
+            :basis basis
+            :javac-opts ["-source" "8" "-target" "8"]}))
