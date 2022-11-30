d2eb456fbcbcffa35647789a09faa2a4f9e16bc1
Huahai Yang <huahai.yang@gmail.com>
2022-11-29 21:19:34 -0800

    Version 0.6.25

--- src/datalevin/main.clj
+++ src/datalevin/main.clj
@@ -28,7 +28,7 @@
   (require 'datalevin.binding.graal)
   (require 'datalevin.binding.java))
 
-(def ^:private version "0.6.24")
+(def ^:private version "0.6.25")
 
 (def ^:private version-str
   (str
