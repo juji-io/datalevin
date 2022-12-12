8456fdc3672f20d3d29968f9139116e7890fbffc
Huahai Yang <huahai.yang@gmail.com>
2022-12-12 11:41:01 -0800

    disable

--- native/script/compile.bat
+++ native/script/compile.bat
@@ -9,56 +9,22 @@ set PATH=%GRAALVM_HOME%\bin;%PATH%
 
 call %GRAALVM_HOME%\bin\gu.cmd install native-image
 
-call ..\lein.bat with-profile test0-uberjar do clean, uberjar
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-call %GRAALVM_HOME%\bin\native-image.cmd ^
-  "-R:MaxHeapSize=5g" ^
-  "-jar" "target/test0.uberjar.jar" ^
-  "-H:NativeLinkerOption=legacy_stdio_definitions.lib" ^
-  dtlv-test0
-
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-.\dtlv-test0 -Xmx5g
-
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-del dtlv-test0
-
-REM call ..\lein.bat with-profile test1-uberjar do clean, uberjar
+REM call ..\lein.bat with-profile test0-uberjar do clean, uberjar
 REM if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM call %GRAALVM_HOME%\bin\native-image.cmd ^
 REM   "-R:MaxHeapSize=5g" ^
-REM   "-jar" "target/test1.uberjar.jar" ^
+REM   "-jar" "target/test0.uberjar.jar" ^
 REM   "-H:NativeLinkerOption=legacy_stdio_definitions.lib" ^
-REM   dtlv-test1
+REM   dtlv-test0
 
 REM if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM .\dtlv-test1 -Xmx5g
+REM .\dtlv-test0 -Xmx5g
 
 REM if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM del dtlv-test1
-
-REM call ..\lein.bat with-profile test2-uberjar do clean, uberjar
-REM if %errorlevel% neq 0 exit /b %errorlevel%
-
-REM call %GRAALVM_HOME%\bin\native-image.cmd ^
-REM   "-R:MaxHeapSize=5g" ^
-REM   "-jar" "target/test2.uberjar.jar" ^
-REM   "-H:NativeLinkerOption=legacy_stdio_definitions.lib" ^
-REM   dtlv-test2
-
-REM if %errorlevel% neq 0 exit /b %errorlevel%
-
-REM .\dtlv-test2 -Xmx5g
-
-REM if %errorlevel% neq 0 exit /b %errorlevel%
-
-REM del dtlv-test2
+REM del dtlv-test0
 
 call ..\lein.bat with-profile native-uberjar uberjar
 if %errorlevel% neq 0 exit /b %errorlevel%
