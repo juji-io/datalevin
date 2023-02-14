b049fa590216d3b7595510bf4a33a8468b2bf168
Huahai Yang <huahai.yang@gmail.com>
2023-02-14 08:42:20 -0800

    update bench to 0.8.5

--- bench/bench.clj
+++ bench/bench.clj
@@ -81,11 +81,11 @@
              (str "{:paths [\"src\"]"
                   ":deps {datalevin/datalevin {:local/root \"..\"}
                   org.clojure/clojure   {:mvn/version \"1.11.1\"}
-                  org.lmdbjava/lmdbjava {:mvn/version \"0.8.2\"}
+                  org.lmdbjava/lmdbjava {:mvn/version \"0.8.3\"}
                   com.taoensso/nippy    {:mvn/version \"3.2.0\"}
                   com.cognitect/transit-clj {:mvn/version \"1.0.324\"}
                   me.lemire.integercompression/JavaFastPFOR {:mvn/version \"0.1.12\"}
-                  org.roaringbitmap/RoaringBitmap {:mvn/version \"0.9.37\"}
+                  org.roaringbitmap/RoaringBitmap {:mvn/version \"0.9.39\"}
                   org.eclipse.collections/eclipse-collections {:mvn/version \"11.1.0\"}
                   }}"
                   )
@@ -140,7 +140,7 @@
 (def default-versions
   [;; ["latest" "datomic"]
    ["latest" "datascript"]
-   ["0.8.4" "datalevin"]
+   ["0.8.5" "datalevin"]
    ["latest" "datalevin"]])
 
 
