5be6e128025e04be505b9d04c98d426869b3ac4a
Huahai Yang <huahai.yang@gmail.com>
2023-04-25 13:52:38 -0700

    mapsize

--- src/datalevin/constants.clj
+++ src/datalevin/constants.clj
@@ -229,7 +229,8 @@
     +init-db-size+
     (let [cur-size (/ ^long (u/dir-size file) (* 1024 1024))]
       (some #(when (<= ^long cur-size ^long %)  %)
-            (iterate #(* ^long +buffer-grow-factor+ ^long %) 1)))))
+            (iterate #(* ^long +buffer-grow-factor+ ^long %)
+                     +init-db-size+)))))
 
 ;; client/server
 
