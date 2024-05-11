9346304787be561323bf8e10174dd94b265d758a
Huahai Yang <huahai.yang@gmail.com>
2024-05-10 21:38:24 -0700

    add a test for rules

--- test/datalevin/rules_test.clj
+++ test/datalevin/rules_test.clj
@@ -1,6 +1,6 @@
 (ns datalevin.rules-test
   (:require
-   [clojure.test :refer [deftest testing are use-fixtures]]
+   [clojure.test :refer [deftest testing are is use-fixtures]]
    [datalevin.test.core :as tdc :refer [db-fixture]]
    [datalevin.rules :as sut]
    [datalevin.core :as d]
@@ -39,18 +39,38 @@
      :j [:c]}
     [#{:e} #{:b} #{:a} #{:h :i :g} #{:c :j :f :d}]))
 
-#_(deftest reachability-data-test
-    (let [dir (u/tmp-dir (str "reachability-test-" (UUID/randomUUID)))
-          db  (-> (d/empty-db
-                    dir
-                    {:node {:db/unique    :db.unique/identity
-                            :db/valueType :db.type/symbol}
-                     :link {:db/cardinality :db.cardinality/many
-                            :db/valueType   :db.type/ref}}
-                    {:kv-opts {:flags (conj c/default-env-flags :nosync)}})
-                  (d/db-with [{:db/id -1 :node 'a :link -2}
-                              {:db/id -2 :node 'b :link -3}
-                              {:db/id -3 :node 'c :link [-3 -4]}
-                              {:db/id -4 :node 'd}]))]
-      (d/close-db db)
-      (u/delete-files dir)))
+(deftest reachability-data-test
+  (let [dir (u/tmp-dir (str "reachability-test-" (UUID/randomUUID)))
+        db  (-> (d/empty-db
+                  dir
+                  {:node {:db/unique    :db.unique/identity
+                          :db/valueType :db.type/symbol}
+                   :link {:db/cardinality :db.cardinality/many
+                          :db/valueType   :db.type/ref}}
+                  {:kv-opts {:flags (conj c/default-env-flags :nosync)}})
+                (d/db-with [{:db/id -1 :node 'a :link -2}
+                            {:db/id -2 :node 'b :link -3}
+                            {:db/id -3 :node 'c :link [-3 -4]}
+                            {:db/id -4 :node 'd}]))]
+    (is (= #{['a 'b]
+             ['b 'c]
+             ['c 'c]
+             ['c 'd]
+             ['a 'c]
+             ['b 'd]
+             ['a 'd]}
+           (d/q '[:find ?n ?m
+                  :in $ %
+                  :where
+                  (reachable ?X ?Y)
+                  [?X :node ?n]
+                  [?Y :node ?m]]
+                db '[[(reachable ?X ?Y)
+                      [?X :link ?Y]]
+                     [(reachable ?X ?Y)
+                      [?X :link ?Z]
+                      (reachable ?Z ?Y)]])))
+    (d/close-db db)
+    (u/delete-files dir)))
+
+#_(deftest math-genealogy-test)
