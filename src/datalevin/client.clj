258cc15eec753f453e79a7f879cd747426e41509
Huahai Yang <huahai.yang@gmail.com>
2025-04-21 23:02:20 -0700

    add license header

--- src/datalevin/client.clj
+++ src/datalevin/client.clj
@@ -1,3 +1,12 @@
+;;
+;; Copyright (c) Huahai Yang. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 2.0 (https://opensource.org/license/epl-2-0)
+;; which can be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+;;
 (ns datalevin.client
   "Datalevin client to Datalevin server, blocking API, with a connection pool"
   (:require
